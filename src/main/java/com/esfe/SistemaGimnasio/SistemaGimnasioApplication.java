90149d753706c187fd59857e52818840ecddd349
Gabriel <gabrielobandogarcia100@gmail.com>
2026-08-11 00:33:16 -0600

    Commit inicial del proyecto Spring Boot

---
+++ src/main/java/com/esfe/SistemaGimnasio/SistemaGimnasioApplication.java
@@ -0,0 +1,13 @@
+package com.esfe.SistemaGimnasio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaGimnasioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaGimnasioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/esfe/SistemaGimnasio/SistemaGimnasioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.esfe.SistemaGimnasio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaGimnasioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
