c6664b715f3b30ed8aefb1454709b3c65125dc9e
Oscar Ramirez <barleyunmain@gmail.com>
2026-08-18 11:38:41 -0600

    SCRUM-160 Oscar Ramirez : Crear Interfaz- se creo el archivo IUsuarioRepositorio

---
+++ src/main/java/com/esfe/sistemagimnasio/repositories/IUsuarioRepository.java
@@ -0,0 +1,8 @@
+package com.esfe.sistemagimnasio.repositories;
+
+import com.esfe.sistemagimnasio.models.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface IUsuarioRepository extends JpaRepository<Usuario, Integer> {
+
+}
