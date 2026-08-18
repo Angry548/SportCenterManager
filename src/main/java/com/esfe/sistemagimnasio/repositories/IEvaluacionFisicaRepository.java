01b00ed6499eb1e93b00ae2c7183a9bd5433636f
Gabriel <gabrielobandogarcia100@gmail.com>
2026-08-18 10:50:53 -0600

    SCRUM-174 Gabriel Obando: Crear interfaz IEvaluacionFisicaRepository

---
+++ src/main/java/com/esfe/sistemagimnasio/repositories/IEvaluacionFisicaRepository.java
@@ -0,0 +1,7 @@
+package com.esfe.sistemagimnasio.repositories;
+
+import com.esfe.sistemagimnasio.models.EvaluacionFisica;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface IEvaluacionFisicaRepository extends JpaRepository<EvaluacionFisica, Integer> {
+}
