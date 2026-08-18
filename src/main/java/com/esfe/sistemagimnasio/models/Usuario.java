0a112fde88395130fad77d159c0c7ecd9743559c
Gabriel <gabrielobandogarcia100@gmail.com>
2026-08-17 21:45:56 -0600

    refactor: agregar validaciones y mapeos a la entidad Usuario

---
+++ src/main/java/com/esfe/sistemagimnasio/models/Usuario.java
@@ -0,0 +1,43 @@
+package com.esfe.sistemagimnasio.models;
+
+import com.esfe.sistemagimnasio.enums.Rol;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table (name = "USUARIO")
+public class Usuario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @NotBlank(message = "El correo es obligatorio")
+    @Email(message = "El correo no tiene un formato valido")
+    @Size(max = 100, message = "El correo no puede exceder 100 caracteres")
+    @Column (name = "email", nullable = false, length = 100,unique = true)
+    private String email;
+
+    @NotBlank(message = "El password es obligatorio")
+    @Size(min = 60, max = 60, message = "El hash de password debe tener 60 caracteres")
+    @Column(name = "password_hash", nullable = false, length = 60)
+    private String passwordHash;
+
+    @NotNull(message = "El rol es obligatorio")
+    @Enumerated(EnumType.STRING)
+    @Column(name = "rol", nullable = false, length = 20)
+    private Rol rol;
+
+    @NotNull
+    @Column(name = "activo", nullable = false)
+    private Boolean activo = true;
+
+    @Column(name = "fecha_registro", nullable = false, updatable = false)
+    private LocalDateTime fechaRegistro;
+
+}
