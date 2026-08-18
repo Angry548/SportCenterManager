2f5b99803c98f43f89c52cc2977cb0a4f6e24299
Gabriel <gabrielobandogarcia100@gmail.com>
2026-08-18 00:21:50 -0600

    refactor: agregar validaciones y mapeos a la entidad Asistencia

--- src/main/java/com/esfe/sistemagimnasio/models/Asistencia.java
+++ src/main/java/com/esfe/sistemagimnasio/models/Asistencia.java
@@ -1,79 +1,29 @@
 package com.esfe.sistemagimnasio.models;
 
+
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+
 import java.time.LocalDateTime;
 
 @Entity
-@Table(name = "asistencias")
+@Table(name = "ASISTENCIA")
 public class Asistencia {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
-    private Integer clienteId;
+
+    @NotNull(message = "El cliente es obligatorio")
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "cliente_id", nullable = false, foreignKey = @ForeignKey(name = "FK_asistencia_cliente"))
+    private Cliente cliente;
+
+    @Column(name = "fecha_hora", nullable = false, updatable = false)
     private LocalDateTime fechaHora;
-    private Boolean autorizado;
 
-
-    public Asistencia() {
-    }
-
-
-    public Asistencia(Integer id, Integer clienteId, LocalDateTime fechaHora, Boolean autorizado) {
-        this.setId(id);
-        this.setClienteId(clienteId);
-        this.setFechaHora(fechaHora);
-        this.setAutorizado(autorizado);
-    }
-
-
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public Integer getClienteId() {
-        return clienteId;
-    }
-
-    public void setClienteId(Integer clienteId) {
-
-        if (clienteId == null) {
-            throw new IllegalArgumentException("El ID del cliente es obligatorio.");
-        }
-        if (clienteId <= 0) {
-            throw new IllegalArgumentException("El ID del cliente debe ser un número positivo.");
-        }
-        this.clienteId = clienteId;
-    }
-
-    public LocalDateTime getFechaHora() {
-        return fechaHora;
-    }
-
-    public void setFechaHora(LocalDateTime fechaHora) {
-
-        if (fechaHora == null) {
-            throw new IllegalArgumentException("La fecha y hora son obligatorias.");
-        }
-        if (fechaHora.isAfter(LocalDateTime.now())) {
-            throw new IllegalArgumentException("La fecha y hora no pueden ser futuras.");
-        }
-        this.fechaHora = fechaHora;
-    }
-
-    public Boolean getAutorizado() {
-        return autorizado;
-    }
-
-    public void setAutorizado(Boolean autorizado) {
-
-        if (autorizado == null) {
-            throw new IllegalArgumentException("El estado de autorización es obligatorio.");
-        }
-        this.autorizado = autorizado;
-    }
+//    @NotNull(message = "El resultado es obligatorio")
+//    @Enumerated(EnumType.STRING)
+//    @Column(name = "resultado", nullable = false, length = 15)
+//    private Resultado resultado;
 }
