53e17ef5f83f52880fa4ad04c459bc6408afdb52
karla <karlarosauras@gmail.com>
2026-08-18 10:38:59 -0600

    [SCRUM-163] Karla Sánchez: Crear interfaz IEntrenadorRepository.

---
+++ src/main/java/com/esfe/sistemagimnasio/repositories/IEntrenadorRepository.java
@@ -0,0 +1,8 @@
+package com.esfe.sistemagimnasio.repositories;
+
+import com.esfe.sistemagimnasio.models.Entrenador;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface IEntrenadorRepository extends JpaRepository<Entrenador, Integer> {
+
+}
