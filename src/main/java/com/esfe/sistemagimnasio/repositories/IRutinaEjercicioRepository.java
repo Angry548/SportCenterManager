871c9dc337093cf9d00c7bcb9849a2c52f784fc0
karla <karlarosauras@gmail.com>
2026-08-18 11:21:27 -0600

    [SCRUM-170] Karla Sánchez: Crear interfaz IRutinaEjercicioRepository.

---
+++ src/main/java/com/esfe/sistemagimnasio/repositories/IRutinaEjercicioRepository.java
@@ -0,0 +1,7 @@
+package com.esfe.sistemagimnasio.repositories;
+
+import com.esfe.sistemagimnasio.models.RutinaEjercicio;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface IRutinaEjercicioRepository extends JpaRepository<RutinaEjercicio,Integer> {
+}
