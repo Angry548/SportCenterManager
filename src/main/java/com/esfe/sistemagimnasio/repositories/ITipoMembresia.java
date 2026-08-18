6f7303d0bab1a446a14f9c1d07e3d2bc9d8ac7b2
Oscar Ramirez <barleyunmain@gmail.com>
2026-08-18 15:37:35 -0600

    SCRUM-162 Oscar Ramirez : Crear Interfaz- se creo el archivo ITipoMembresia.java

---
+++ src/main/java/com/esfe/sistemagimnasio/repositories/ITipoMembresia.java
@@ -0,0 +1,8 @@
+package com.esfe.sistemagimnasio.repositories;
+
+import com.esfe.sistemagimnasio.models.TipoMembresia;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ITipoMembresiaRepository extends JpaRepository<TipoMembresia, Integer> {
+
+}
