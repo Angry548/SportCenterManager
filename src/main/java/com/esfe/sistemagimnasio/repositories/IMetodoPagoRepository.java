5cb1a416b4ba6e0593e3f3d684c999c92f71668e
Andrea <denissedepaz16@gmail.com>
2026-08-18 11:35:44 -0600

    SCRUM-171-Andrea De Paz - Crear Interfaz de IMetodoPagoRepository

---
+++ src/main/java/com/esfe/sistemagimnasio/repositories/IMetodoPagoRepository.java
@@ -0,0 +1,7 @@
+package com.esfe.sistemagimnasio.repositories;
+
+import com.esfe.sistemagimnasio.models.MetodoPago;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface IMetodoPagoRepository extends JpaRepository<MetodoPago, Integer> {
+}
