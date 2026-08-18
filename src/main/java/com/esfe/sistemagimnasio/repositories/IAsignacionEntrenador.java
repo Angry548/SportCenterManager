477263033436c96fcd78fe5a06c179fa3bf05fcf
karla <karlarosauras@gmail.com>
2026-08-18 11:01:53 -0600

    [SCRUM-167] Karla Sánchez: Crear interfaz IAsignacionEntrenadorRepository.

---
+++ src/main/java/com/esfe/sistemagimnasio/repositories/IAsignacionEntrenador.java
@@ -0,0 +1,7 @@
+package com.esfe.sistemagimnasio.repositories;
+
+import com.esfe.sistemagimnasio.models.AsignacionEntrenador;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface IAsignacionEntrenador extends JpaRepository<AsignacionEntrenador, Integer> {
+}
