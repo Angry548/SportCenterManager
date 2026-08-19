1e47bbee1df9ec82dc5b94026070bce80ecca031
Oscar Ramirez <barleyunmain@gmail.com>
2026-08-19 11:34:08 -0600

    SCRUM-162 Oscar Ramirez : Crear la logica - se desarrollo la logica de ITipoMembresiaRepository.java

--- src/main/java/com/esfe/sistemagimnasio/repositories/ITipoMembresia.java
+++ src/main/java/com/esfe/sistemagimnasio/repositories/ITipoMembresia.java
@@ -3,6 +3,10 @@ package com.esfe.sistemagimnasio.repositories;
 import com.esfe.sistemagimnasio.models.TipoMembresia;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ITipoMembresiaRepository extends JpaRepository<TipoMembresia, Integer> {
+
+    List<TipoMembresia> findByActivoTrue();
 
 }
