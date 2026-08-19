7c26a3a2323329f6d66a821b85df55a932300179
Andrea <denissedepaz16@gmail.com>
2026-08-19 15:07:16 -0600

    SCRUM-168-Andrea De Paz - Se implemento  logica IMetodoPagoRepository

--- src/main/java/com/esfe/sistemagimnasio/repositories/IMetodoPagoRepository.java
+++ src/main/java/com/esfe/sistemagimnasio/repositories/IMetodoPagoRepository.java
@@ -2,6 +2,8 @@ package com.esfe.sistemagimnasio.repositories;
 
 import com.esfe.sistemagimnasio.models.MetodoPago;
 import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
 
 public interface IMetodoPagoRepository extends JpaRepository<MetodoPago, Integer> {
+    List<MetodoPago> findByActivoTrue();
 }
