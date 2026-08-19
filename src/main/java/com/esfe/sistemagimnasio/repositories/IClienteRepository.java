35cf9ad29b98b7f7220549be439d0dd66b66713e
Oscar Ramirez <barleyunmain@gmail.com>
2026-08-19 11:38:37 -0600

    SCRUM-161 Oscar Ramirez : Crear la logica - se desarrollo la logica de IClienteRepository.java

--- src/main/java/com/esfe/sistemagimnasio/repositories/IClienteRepository.java
+++ src/main/java/com/esfe/sistemagimnasio/repositories/IClienteRepository.java
@@ -3,6 +3,14 @@ package com.esfe.sistemagimnasio.repositories;
 import com.esfe.sistemagimnasio.models.Cliente;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface IClienteRepository extends JpaRepository<Cliente, Integer> {
+
+    Optional<Cliente> findByDui(String dui);
+
+    Optional<Cliente> findByUsuario_Id(Integer usuarioId);
+
+    Optional<Cliente> findByUsuario_Email(String email);
 
 }
