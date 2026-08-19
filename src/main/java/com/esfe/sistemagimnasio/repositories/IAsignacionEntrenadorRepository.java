3017fb59eefc171c34feaf73f9922b87738ad828
karla <karlarosauras@gmail.com>
2026-08-19 10:50:01 -0600

    [SCRUM-167] Karla Sánchez: Implementar logica de consulta de AsignacionEntrenador

--- src/main/java/com/esfe/sistemagimnasio/repositories/IAsignacionEntrenadorRepository.java
+++ src/main/java/com/esfe/sistemagimnasio/repositories/IAsignacionEntrenadorRepository.java
@@ -3,6 +3,10 @@ package com.esfe.sistemagimnasio.repositories;
 import com.esfe.sistemagimnasio.models.AsignacionEntrenador;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface IAsignacionEntrenadorRepository extends JpaRepository<AsignacionEntrenador, Integer> {
 
+    List<AsignacionEntrenador> findByEntrenador_IdAndFechaFinIsNull(Integer entrenadorId);
+    List<AsignacionEntrenador> findByCliente_IdOrderByFechaAsignacionDesc(Integer clienteId);
 }
