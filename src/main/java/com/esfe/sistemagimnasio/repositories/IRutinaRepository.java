f820ac0f1d594bab0d6ab4a35ae8f9230080fd8b
Gabriel <gabrielobandogarcia100@gmail.com>
2026-08-18 19:39:11 -0600

    SCRUM-172 Gabriel Obando: Crear interfaz IRutinaRepository

---
+++ src/main/java/com/esfe/sistemagimnasio/repositories/IRutinaRepository.java
@@ -0,0 +1,7 @@
+package com.esfe.sistemagimnasio.repositories;
+
+import com.esfe.sistemagimnasio.models.Rutina;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface IRutinaRepository extends JpaRepository<Rutina,Integer> {
+}
