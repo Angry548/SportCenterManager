856ea19400572821081c90cb46ecd15886e43b6b
karla <karlarosauras@gmail.com>
2026-08-19 11:17:13 -0600

    [SCRUM-170] Karla Sánchez: Implementar logica de consulta de RutinaEjercicio

--- src/main/java/com/esfe/sistemagimnasio/repositories/IRutinaEjercicioRepository.java
+++ src/main/java/com/esfe/sistemagimnasio/repositories/IRutinaEjercicioRepository.java
@@ -3,5 +3,10 @@ package com.esfe.sistemagimnasio.repositories;
 import com.esfe.sistemagimnasio.models.RutinaEjercicio;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface IRutinaEjercicioRepository extends JpaRepository<RutinaEjercicio,Integer> {
+
+    List<RutinaEjercicio> findByRutina_Id(Integer rutinaId);
+    boolean existsByEjercicio_Id(Integer ejercicioId);
 }
