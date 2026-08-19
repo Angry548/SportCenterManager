bd4af6ddebb5e754c270830f14ecad30d11cb59b
Andrea <denissedepaz16@gmail.com>
2026-08-19 14:59:52 -0600

    SCRUM-168-Andrea De Paz - Se implemento  logica en Asistencia

--- src/main/java/com/esfe/sistemagimnasio/repositories/IAsistenciaRepository.java
+++ src/main/java/com/esfe/sistemagimnasio/repositories/IAsistenciaRepository.java
@@ -1,7 +1,12 @@
 package com.esfe.sistemagimnasio.repositories;
 
 import com.esfe.sistemagimnasio.models.Asistencia;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface IAsistenciaRepository extends JpaRepository<Asistencia, Integer> {
+    Page<Asistencia> findByCliente_IdOrderByFechaHoraDesc(Integer clienteId, Pageable pageable);
+
+    long countByCliente_IdAndResultado(Integer clienteId, Resultado resultado);
 }
