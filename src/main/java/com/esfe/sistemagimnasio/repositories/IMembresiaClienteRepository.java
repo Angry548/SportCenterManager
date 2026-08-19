eed20d627cdfbb2120a76cfc78faa3d228455b04
Gabriel <gabrielobandogarcia100@gmail.com>
2026-08-19 09:26:15 -0600

    SCRUM-159 Gabriel Obando: Implementar lógica de consulta de membresías

--- src/main/java/com/esfe/sistemagimnasio/repositories/IMembresiaClienteRepository.java
+++ src/main/java/com/esfe/sistemagimnasio/repositories/IMembresiaClienteRepository.java
@@ -3,7 +3,9 @@ package com.esfe.sistemagimnasio.repositories;
 import com.esfe.sistemagimnasio.models.MembresiaCliente;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 
 public interface IMembresiaClienteRepository extends JpaRepository<MembresiaCliente, Integer> {
-
+    List<MembresiaCliente> findByCliente_IdOrderByFechaVencimientoDesc(Integer clienteId);
 }
