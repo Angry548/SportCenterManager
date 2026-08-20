1be6c45e53a2367d222d53b4ebccd3118779b52b
Gabriel <gabrielobandogarcia100@gmail.com>
2026-08-19 19:44:25 -0600

    SCRUM-174 Gabriel Obando: Implementar lógica de consulta de evaluacion fisica

--- src/main/java/com/esfe/sistemagimnasio/repositories/IEvaluacionFisicaRepository.java
+++ src/main/java/com/esfe/sistemagimnasio/repositories/IEvaluacionFisicaRepository.java
@@ -3,5 +3,8 @@ package com.esfe.sistemagimnasio.repositories;
 import com.esfe.sistemagimnasio.models.EvaluacionFisica;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface IEvaluacionFisicaRepository extends JpaRepository<EvaluacionFisica, Integer> {
+    List<EvaluacionFisica> findByCliente_IdOrderByFechaDesc(Integer clienteId);
 }
