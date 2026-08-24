fa88052120642d89a08b6dee89a4e1e2a0194e15
Oscar Ramirez <barleyunmain@gmail.com>
2026-08-24 11:23:18 -0600

    SCRUM-183 Oscar Andres Ramirez Romero : Crear el servicio - se desarrollo la logica de servicio a IClienteService.java

--- src/main/java/com/esfe/sistemagimnasio/services/interfaces/IClienteService.java
+++ src/main/java/com/esfe/sistemagimnasio/services/interfaces/IClienteService.java
@@ -1,4 +1,21 @@
 package com.esfe.sistemagimnasio.services.interfaces;
 
+import com.esfe.sistemagimnasio.models.Cliente;
+
+import java.util.List;
+import java.util.Optional;
+
 public interface IClienteService {
+
+    List<IClienteService> obtenerTodos();
+
+    Optional<IClienteService> obtenerPorId(Integer id);
+
+    Cliente guardar(Cliente cliente);
+
+    void eliminar(Integer id);
+
+    boolean tieneMembresiaVigente(Integer clienteId);
+
+    boolean tieneEntrenadorActivo(Integer clienteId);
 }
