c148b1a32446bd54bab80aa84978090cf2366f0d
Oscar Ramirez <barleyunmain@gmail.com>
2026-08-25 11:31:14 -0600

    SCRUM-182 Oscar Andres Ramirez Romero : Crear el servicio - se desarrollo la logica de servicio a IUsuarioService.java

--- src/main/java/com/esfe/sistemagimnasio/services/interfaces/IUsuarioService.java
+++ src/main/java/com/esfe/sistemagimnasio/services/interfaces/IUsuarioService.java
@@ -7,20 +7,6 @@ import java.util.Optional;
 
 public interface IUsuarioService {
 
-    // Métodos de Usuario
-    List<IUsuarioService> obtenerTodos();
-
-    Optional<IUsuarioService> obtenerPorId(Integer id);
-
-    Usuario guardar(Usuario usuario);
-
-    void eliminar(Integer id);
-
-    Optional<IUsuarioService> autenticar(String email, String password);
-
-    Usuario registrarUsuario(Usuario usuario);
-
-    // Métodos de Membresia
     List<Usuario> obtenerTodasMembresias();
 
     Optional<Usuario> obtenerMembresiaPorId(Integer id);
