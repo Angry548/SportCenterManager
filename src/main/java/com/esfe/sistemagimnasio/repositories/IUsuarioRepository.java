fcfa78aeac1a906200f4e7dc268027b3e1defff5
Oscar Ramirez <barleyunmain@gmail.com>
2026-08-26 09:29:04 -0600

    SCRUM-160 Oscar Andres Ramirez Romero : Se modifico el archivo IUsuarioRepository.java

--- src/main/java/com/esfe/sistemagimnasio/repositories/IUsuarioRepository.java
+++ src/main/java/com/esfe/sistemagimnasio/repositories/IUsuarioRepository.java
@@ -3,13 +3,7 @@ package com.esfe.sistemagimnasio.repositories;
 import com.esfe.sistemagimnasio.models.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.Optional;
-
 public interface IUsuarioRepository extends JpaRepository<Usuario, Integer> {
-
-    Optional<Usuario> findByEmail(String email);
-
-    boolean existsByEmail(String email);
 
 }
 
