b83e606680423b05d567f630343a17f2b11559d6
Gabriel <gabrielobandogarcia100@gmail.com>
2026-08-27 10:10:47 -0600

    SCRUM-191 Gabriel Obando: refactor - Actualizar interfaz e implementacion del servicio EvaluacionFisica

--- src/main/java/com/esfe/sistemagimnasio/services/interfaces/IEvaluacionFisicaService.java
+++ src/main/java/com/esfe/sistemagimnasio/services/interfaces/IEvaluacionFisicaService.java
@@ -1,7 +1,10 @@
 package com.esfe.sistemagimnasio.services.interfaces;
 
 import com.esfe.sistemagimnasio.models.EvaluacionFisica;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
+import java.math.BigDecimal;
 import java.util.List;
 import java.util.Optional;
 
@@ -14,4 +17,8 @@ public interface IEvaluacionFisicaService {
     EvaluacionFisica guardar (EvaluacionFisica evaluacionFisica);
 
     void eliminar (Integer id);
+
+    Page<EvaluacionFisica> obtenerTodosPaginados(Pageable pageable);
+
+    double calcularIMC(Integer id);
 }
