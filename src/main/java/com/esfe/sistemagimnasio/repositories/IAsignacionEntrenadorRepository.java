de1c302d53d56ce0409c15e8106bab2d5b63e77d
Gabriel <gabrielobandogarcia100@gmail.com>
2026-08-27 11:42:07 -0600

    SCRUM-178 Gabriel Obando: refactor - Actualizar interfaz e implementación del servicio AsignacionEntrenador

--- src/main/java/com/esfe/sistemagimnasio/repositories/IAsignacionEntrenadorRepository.java
+++ src/main/java/com/esfe/sistemagimnasio/repositories/IAsignacionEntrenadorRepository.java
@@ -6,5 +6,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface IAsignacionEntrenadorRepository extends JpaRepository<AsignacionEntrenador, Integer> {
-
+    boolean existsByClienteIdAndFechaFinIsNull(Integer clienteId);
 }
