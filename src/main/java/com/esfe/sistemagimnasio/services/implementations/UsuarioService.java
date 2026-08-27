1e0a0ebc16518cf241214597ce95207257b1c21d
Gabriel <gabrielobandogarcia100@gmail.com>
2026-08-27 10:39:56 -0600

    SCRUM-182 Gabriel Obando: refactor - Actualizar interfaz e implementacion del servicio Usuario

--- src/main/java/com/esfe/sistemagimnasio/services/implementations/UsuarioService.java
+++ src/main/java/com/esfe/sistemagimnasio/services/implementations/UsuarioService.java
@@ -5,6 +5,8 @@ import com.esfe.sistemagimnasio.repositories.IUsuarioRepository;
 import com.esfe.sistemagimnasio.services.interfaces.IClienteService;
 import com.esfe.sistemagimnasio.services.interfaces.IUsuarioService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -35,5 +37,44 @@ public class UsuarioService implements IUsuarioService {
         usuarioRepository.deleteById(id);
 
     }
+
+    @Override
+    public Page<Usuario> obtenerTodosPaginados(Pageable pageable) {
+        return usuarioRepository.findAll(pageable);
+    }
+
+    //Se modificarán más adelante apropiadamente para SpringSecurity
+    @Override
+    public boolean autenticar(Integer id, String password) {
+        Usuario usuario = obtenerPorId(id).orElseThrow();
+
+        return usuario.getPasswordHash().equals(password);
+    }
+
+    @Override
+    public void cambiarPassword(Integer id, String nuevaPassword) {
+        Usuario usuario = obtenerPorId(id).orElseThrow();
+
+        usuario.setPasswordHash(nuevaPassword);
+        guardar(usuario);
+    }
+
+    @Override
+    public void activar(Integer id) {
+        Usuario usuario = obtenerPorId(id).orElseThrow();
+
+        usuario.setActivo(true);
+        guardar(usuario);
+    }
+
+    @Override
+    public void desactivar(Integer id) {
+        Usuario usuario = obtenerPorId(id).orElseThrow();
+
+        usuario.setActivo(false);
+        guardar(usuario);
+    }
+
+
 }
 
--- src/main/java/com/esfe/sistemagimnasio/services/interfaces/IUsuarioService.java
+++ src/main/java/com/esfe/sistemagimnasio/services/interfaces/IUsuarioService.java
@@ -1,6 +1,8 @@
 package com.esfe.sistemagimnasio.services.interfaces;
 
 import com.esfe.sistemagimnasio.models.Usuario;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 import java.util.List;
 import java.util.Optional;
@@ -14,6 +16,16 @@ public interface IUsuarioService {
     Usuario guardar (Usuario usuario);
 
     void eliminar(Integer id);
+
+    Page<Usuario> obtenerTodosPaginados(Pageable pageable);
+
+    boolean autenticar(Integer id, String password);
+
+    void cambiarPassword(Integer id, String nuevaPassword);
+
+    void activar(Integer id);
+
+    void desactivar(Integer id);
 }
 
 
