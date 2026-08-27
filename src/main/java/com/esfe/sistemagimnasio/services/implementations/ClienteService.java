72442f05f538632856a0cf5b6c73e00ea6ba0e55
Gabriel <gabrielobandogarcia100@gmail.com>
2026-08-27 11:01:47 -0600

    SCRUM-183 Gabriel Obando: refactor - Actualizar interfaz e implementación del servicio Cliente

--- src/main/java/com/esfe/sistemagimnasio/services/implementations/ClienteService.java
+++ src/main/java/com/esfe/sistemagimnasio/services/implementations/ClienteService.java
@@ -4,14 +4,24 @@ import com.esfe.sistemagimnasio.models.Cliente;
 import com.esfe.sistemagimnasio.repositories.IClienteRepository;
 import com.esfe.sistemagimnasio.services.interfaces.IClienteService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDate;
+import java.time.Period;
 import java.util.List;
 import java.util.Optional;
 @Service
 public class ClienteService implements IClienteService {
     @Autowired
     IClienteRepository clienteRepository;
+
+    @Autowired
+    private IMembresiaClienteRepository membresiaClienteRepository;
+
+    @Autowired
+    private IAsignacionEntrenadorRepository asignacionEntrenadorRepository;
 
     @Override
     public List<Cliente> obtenerTodos() {
@@ -32,5 +42,35 @@ public class ClienteService implements IClienteService {
     public void eliminar(Integer id) {
         clienteRepository.deleteById(id);
 
+    }
+
+    @Override
+    public Page<Cliente> obtenerTodosPaginados(Pageable pageable) {
+        return clienteRepository .findAll(pageable);
+    }
+
+    @Override
+    public int obtenerEdad(Integer id) {
+        Cliente cliente = obtenerPorId(id).orElseThrow();
+
+        return Period.between(
+                cliente.getFechaNacimiento(),
+                LocalDate.now()
+        ).getYears();
+    }
+
+    @Override
+    public boolean tieneMembresiaVigente(Integer id) {
+        return membresiaClienteRepository
+                .existsByClienteIdAndFechaVencimientoGreaterThanEqual(
+                        id,
+                        LocalDate.now()
+                );
+    }
+
+    @Override
+    public boolean tieneEntrenadorActivo(Integer id) {
+        return asignacionEntrenadorRepository
+                .existsByClienteIdAndFechaFinIsNull(id);
     }
 }
--- src/main/java/com/esfe/sistemagimnasio/services/interfaces/IClienteService.java
+++ src/main/java/com/esfe/sistemagimnasio/services/interfaces/IClienteService.java
@@ -2,6 +2,8 @@ package com.esfe.sistemagimnasio.services.interfaces;
 
 
 import com.esfe.sistemagimnasio.models.Cliente;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 import java.util.List;
 import java.util.Optional;
@@ -15,5 +17,13 @@ public interface IClienteService {
     Cliente guardar(Cliente cliente);
 
     void eliminar(Integer id);
+
+    Page<Cliente> obtenerTodosPaginados(Pageable pageable);
+
+    int obtenerEdad(Integer id);
+
+    boolean tieneMembresiaVigente(Integer id);
+
+    boolean tieneEntrenadorActivo(Integer id);
 }
 
