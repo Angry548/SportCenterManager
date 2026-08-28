239de763e8be2cbcf6e5f397c358d18a05241a92
Gabriel <gabrielobandogarcia100@gmail.com>
2026-08-28 16:07:24 -0600

    SCRUM-177 Gabriel Obando: refactor - Actualizar interfaz e implementación del servicio Entrenador

--- src/main/java/com/esfe/sistemagimnasio/services/interfaces/IEntrenadorService.java
+++ src/main/java/com/esfe/sistemagimnasio/services/interfaces/IEntrenadorService.java
@@ -1,13 +1,24 @@
 package com.esfe.sistemagimnasio.services.interfaces;
 
+import com.esfe.sistemagimnasio.models.Cliente;
 import com.esfe.sistemagimnasio.models.Entrenador;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 import java.util.List;
 import java.util.Optional;
 
 public interface IEntrenadorService {
+
     List<Entrenador> obtenerTodos();
-    Optional<Entrenador> obtenerPorld(Integer Id);
+
+    Optional<Entrenador> obtenerPorId(Integer Id);
+
     Entrenador guardar (Entrenador entrenador);
+
     void eliminar (Integer id);
+
+    Page<Entrenador> obtenerTodosPaginados(Pageable pageable);
+
+    List<Cliente> obtenerClientesActivos(Integer id);
 }
