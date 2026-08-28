f4a126eaaca85bc499b47d270375fa9f430ce116
Gabriel <gabrielobandogarcia100@gmail.com>
2026-08-28 15:49:00 -0600

    SCRUM-187 Gabriel Obando: refactor - Actualizar interfaz e implementación del servicio MetodoPago

--- src/main/java/com/esfe/sistemagimnasio/services/interfaces/IMetodoPagoService.java
+++ src/main/java/com/esfe/sistemagimnasio/services/interfaces/IMetodoPagoService.java
@@ -1,6 +1,8 @@
 package com.esfe.sistemagimnasio.services.interfaces;
 
 import com.esfe.sistemagimnasio.models.MetodoPago;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 import java.util.List;
 import java.util.Optional;
@@ -8,9 +10,17 @@ import java.util.Optional;
 public interface IMetodoPagoService {
 
         List<MetodoPago> obtenerTodos();
-        List<MetodoPago> obtenerActivos();
+
         Optional<MetodoPago> obtenerPorId(Integer id);
+
         MetodoPago guardar(MetodoPago metodoPago);
+
         void eliminar(Integer id);
+
+        Page<MetodoPago> obtenerTodosPaginados(Pageable pageable);
+
+        void activar(Integer id);
+
+        void desactivar(Integer id);
     }
 
