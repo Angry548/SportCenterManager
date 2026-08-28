301f55829fc0a74d0fb0fe8aaaea67bbc868a09a
Gabriel <gabrielobandogarcia100@gmail.com>
2026-08-28 16:12:06 -0600

    refactor - Actualizar capa repository

--- src/main/java/com/esfe/sistemagimnasio/repositories/IAsignacionEntrenadorRepository.java
+++ src/main/java/com/esfe/sistemagimnasio/repositories/IAsignacionEntrenadorRepository.java
@@ -1,11 +1,21 @@
 package com.esfe.sistemagimnasio.repositories;
 
 import com.esfe.sistemagimnasio.models.AsignacionEntrenador;
+import com.esfe.sistemagimnasio.models.Cliente;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 import java.util.Optional;
 
 public interface IAsignacionEntrenadorRepository extends JpaRepository<AsignacionEntrenador, Integer> {
     boolean existsByClienteIdAndFechaFinIsNull(Integer clienteId);
+
+    @Query("""
+    SELECT a.cliente
+    FROM AsignacionEntrenador a
+    WHERE a.entrenador.id = :entrenadorId
+    AND a.fechaFin IS NULL
+""")
+    List<Cliente> obtenerClientesActivos(Integer entrenadorId);
 }
