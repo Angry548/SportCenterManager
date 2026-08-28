d90def7bad9de4de064130d2f44f27c963e4f111
Gabriel <gabrielobandogarcia100@gmail.com>
2026-08-28 16:07:24 -0600

    SCRUM-177 Gabriel Obando: refactor - Actualizar interfaz e implementación del servicio Entrenador

--- src/main/java/com/esfe/sistemagimnasio/repositories/IAsistenciaRepository.java
+++ src/main/java/com/esfe/sistemagimnasio/repositories/IAsistenciaRepository.java
@@ -6,7 +6,5 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface IAsistenciaRepository extends JpaRepository<Asistencia, Integer> {
-    Page<Asistencia> findByCliente_IdOrderByFechaHoraDesc(Integer clienteId, Pageable pageable);
 
-    long countByCliente_IdAndResultado(Integer clienteId, Resultado resultado);
 }
--- src/main/java/com/esfe/sistemagimnasio/services/implementations/EntrenadorService.java
+++ src/main/java/com/esfe/sistemagimnasio/services/implementations/EntrenadorService.java
@@ -1,9 +1,13 @@
 package com.esfe.sistemagimnasio.services.implementations;
 
+import com.esfe.sistemagimnasio.models.Cliente;
 import com.esfe.sistemagimnasio.models.Entrenador;
+import com.esfe.sistemagimnasio.repositories.IAsignacionEntrenadorRepository;
 import com.esfe.sistemagimnasio.repositories.IEntrenadorRepository;
 import com.esfe.sistemagimnasio.services.interfaces.IEntrenadorService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -15,14 +19,17 @@ public class EntrenadorService implements IEntrenadorService {
     @Autowired
     private IEntrenadorRepository entrenadorRepository;
 
+    @Autowired
+    private IAsignacionEntrenadorRepository asignacionEntrenadorRepository;
+
     @Override
     public List<Entrenador> obtenerTodos() {
         return entrenadorRepository.findAll();
     }
 
     @Override
-    public Optional<Entrenador> obtenerPorld(Integer id) {
-        return entrenadorRepository.findById(id);
+    public Optional<Entrenador> obtenerPorId(Integer Id) {
+        return entrenadorRepository.findById(Id);
     }
 
     @Override
@@ -33,5 +40,15 @@ public class EntrenadorService implements IEntrenadorService {
     @Override
     public void eliminar(Integer id) {
         entrenadorRepository.deleteById(id);
+    }
+
+    @Override
+    public Page<Entrenador> obtenerTodosPaginados(Pageable pageable) {
+        return entrenadorRepository.findAll(pageable);
+    }
+
+    @Override
+    public List<Cliente> obtenerClientesActivos(Integer id) {
+        return asignacionEntrenadorRepository.obtenerClientesActivos(id);
     }
 }
